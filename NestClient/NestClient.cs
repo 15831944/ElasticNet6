2efd4833b976a12616bbe81d406013cd8db8bd23
Neomaster <the@neomaster.net>
2022-05-05 21:40:53 +0300

    0.3.0.0 NestClient

---
+++ NestClient/NestClient.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Elasticsearch.Net;
+using Nest;
+
+namespace Elastic;
+
+public class NestClient
+{
+#nullable disable
+    private ElasticClient elasticClient;
+    public ElasticClient ElasticClient => elasticClient;
+    public readonly List<Node> Nodes = new();
+    public readonly string ElasticsearchUserName;
+    public readonly string HttpsCertificatePath;
+    private readonly string elasticsearchUserPassword;
+#nullable enable
+    private readonly string? httpsCertificatePassword;
+
+    public NestClient(
+        IEnumerable<string> nodesUrls,
+        string elasticsearchUserName,
+        string elasticsearchUserPassword,
+        string httpsCertificatePath,
+        string? httpsCertificatePassword
+    )
+    {
+        foreach (string url in nodesUrls)
+        {
+            Nodes.Add(new Node(new Uri(url)));
+        }
+
+        ElasticsearchUserName = elasticsearchUserName;
+        HttpsCertificatePath = httpsCertificatePath;
+        this.elasticsearchUserPassword = elasticsearchUserPassword;
+        this.httpsCertificatePassword = httpsCertificatePassword;
+    }
+
+    public bool Connect(out Exception? exception)
+    {
+        StaticConnectionPool connectionPool = new(Nodes);
+        ConnectionSettings connectionSettings = new(connectionPool);
+
+        try
+        {
+            connectionSettings.PingTimeout(TimeSpan.FromSeconds(5));
+            connectionSettings.BasicAuthentication(ElasticsearchUserName, elasticsearchUserPassword);
+            connectionSettings.DisableDirectStreaming(true);
+            connectionSettings.ServerCertificateValidationCallback(
+                (sender, cert, chain, errors) =>
+                {
+                    X509Certificate clientCert = new(HttpsCertificatePath, httpsCertificatePassword);
+                    return cert.GetCertHashString() == clientCert.GetCertHashString();
+                });
+
+            elasticClient = new(connectionSettings);
+
+            exception = null;
+            return elasticClient.Cluster.Stats().IsValid;
+        }
+        catch (Exception ex)
+        {
+            exception = ex;
+            return false;
+        }
+    }
+}
