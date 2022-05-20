d3e8199b009a6d5aa7d8be495d8c554b360f7a04
Neomaster <the@neomaster.net>
2022-05-20 19:47:43 +0300

    0.15.0.0 BulkIndexDocs_BulkResponseCallback

---
+++ BulkIndexDocs_BulkResponseCallback/Program.cs
@@ -0,0 +1,91 @@
+﻿using Elastic;
+using Elastic.Docs;
+using Elastic.Reports;
+
+#region Connection
+NestClient nestClient = new(
+    new string[] { "https://elastic.home:9200" },
+    "elastic",
+    "ela5tic",
+    "C:/elasticsearch-8.1.1/config/http.p12",
+    "http"
+    );
+
+if (nestClient.Connect(out Exception? e))
+{
+    Console.WriteLine("The connection with elasticsearch was successfully established.");
+}
+else
+{
+    Console.WriteLine(e!.Message);
+    return;
+}
+#endregion
+
+static IEnumerable<NameValueDoc<string, string>> GenDocs(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        yield return new NameValueDoc<string, string>
+        {
+            Name = "color",
+            Value = $"#{i}"
+        };
+    }
+}
+
+void Test1_InvalidIndexName()
+{
+    Console.WriteLine("[Test 1: invalid index name]");
+    Console.WriteLine("----------------------------");
+
+    long currentTotal = 0;
+
+    if (!nestClient.BulkIndex("te st", GenDocs(1000), out long _, out long _, out Exception? ex,
+        portionSize: 250,
+        bulkResponseCallback: bulkResponse =>
+        {
+            BulkResponseReport m = new(bulkResponse, ref currentTotal);
+            Console.WriteLine(m);
+        }))
+    {
+        Console.WriteLine("Error: " + ex!.Message);
+    }
+
+    // Output:
+    //{"HasError":true,"CurrentTotal":1000,"ItemsWithErrorsCount":250,"ItemsByStatus":{"400":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"]}
+    //{"HasError":true,"CurrentTotal":500,"ItemsWithErrorsCount":250,"ItemsByStatus":{"400":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"]}
+    //{"HasError":true,"CurrentTotal":250,"ItemsWithErrorsCount":250,"ItemsByStatus":{"400":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"]}
+    //{"HasError":true,"CurrentTotal":750,"ItemsWithErrorsCount":250,"ItemsByStatus":{"400":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"]}
+    //Error: Refreshing after all documents have indexed failed
+
+    // All responses have status 400.
+}
+
+void Test2_CatchingErrors()
+{
+    Console.WriteLine("[Test 2: catching errors]");
+    Console.WriteLine("-------------------------");
+
+    long currentTotal = 0;
+
+    if (!nestClient.BulkIndex("test", GenDocs(1000).Select(doc => doc.Value == "#300" ? throw new Exception("Error on #300.") : doc),
+        out long _, out long _, out Exception? ex,
+        portionSize: 250,
+        bulkResponseCallback: bulkResponse =>
+        {
+            BulkResponseReport m = new(bulkResponse, ref currentTotal);
+            Console.WriteLine(m);
+        }))
+    {
+        Console.WriteLine("Error: " + ex!.Message);
+    }
+
+    // Output:
+    //Error: Error on #300.
+
+    // GET /test/_count --> 250
+}
+
+//Test1_InvalidIndexName();
+Test2_CatchingErrors();
---
+++ NestClient/Reports/BulkResponseReport.cs
@@ -0,0 +1,37 @@
+﻿using Nest;
+using Newtonsoft.Json;
+
+namespace Elastic.Reports;
+
+public class BulkResponseReport
+{
+    public bool HasError;
+    public long? CurrentTotal;
+    public long ItemsWithErrorsCount;
+    public Dictionary<int, int> ItemsByStatus;
+    public List<string> ErrorsReasons;
+
+    public BulkResponseReport(BulkResponse r)
+    {
+        var errors = r.Items
+            .Where(i => i.Error is not null)
+            .Select(i => i.Error.Reason);
+
+        HasError = errors.Any();
+        ItemsWithErrorsCount = errors.Count();
+        ErrorsReasons = errors.Distinct().ToList();
+        ItemsByStatus = r.Items
+            .GroupBy(k => k.Status, v => v)
+            .ToDictionary(k => k.Key, v => v.Count());
+    }
+
+    public BulkResponseReport(BulkResponse r, ref long currentTotal) : this(r)
+    {
+        CurrentTotal = Interlocked.Add(ref currentTotal, r.Items.Count);
+    }
+
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+}
