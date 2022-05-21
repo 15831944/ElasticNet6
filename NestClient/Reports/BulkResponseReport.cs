2439c9b5eeec7f87b06bfb4cfa2413a6b51b3b96
Neomaster <the@neomaster.net>
2022-05-21 16:38:01 +0300

    0.19.0.0 [NestClient] BulkResponseReport. Using ItemsWithErrors, added IsValid, ItemsByIsValid.

--- NestClient/Reports/BulkResponseReport.cs
+++ NestClient/Reports/BulkResponseReport.cs
@@ -6,10 +6,12 @@ namespace Elastic.Reports;
 
 public class BulkResponseReport
 {
+    public bool IsValid;
     public bool HasError;
-    public long? CurrentTotal;
     public long ItemsWithErrorsCount;
+    public long? CurrentTotal;
     public Dictionary<int, int> ItemsByStatus;
+    public Dictionary<bool, int> ItemsByIsValid;
     public List<string> ErrorsReasons;
 
     [JsonIgnore]
@@ -22,19 +24,26 @@ public class BulkResponseReport
 
     public BulkResponseReport(BulkResponse r)
     {
-        var errors = r.Items
-            .Where(i => i.Error is not null)
-            .Select(i => i.Error.Reason);
+        IsValid = r.IsValid;
+        OriginalException = r.OriginalException;
+        ServerError = r.ServerError;
 
-        HasError = errors.Any();
-        ItemsWithErrorsCount = errors.Count();
-        ErrorsReasons = errors.Distinct().ToList();
+        HasError = r.ItemsWithErrors.Any();
+        ItemsWithErrorsCount = r.ItemsWithErrors.Count();
+        
+        ErrorsReasons = r.ItemsWithErrors
+            .Where(i => i.Error?.Reason is not null)
+            .Select(i => i.Error.Reason)
+            .Distinct()
+            .ToList();
+        
         ItemsByStatus = r.Items
             .GroupBy(k => k.Status, v => v)
             .ToDictionary(k => k.Key, v => v.Count());
 
-        OriginalException = r.OriginalException;
-        ServerError = r.ServerError;
+        ItemsByIsValid = r.Items
+            .GroupBy(k => k.IsValid, v => v)
+            .ToDictionary(k => k.Key, v => v.Count());
     }
 
     public BulkResponseReport(BulkResponse r, ref long currentTotal) : this(r)
