a5bd90e3eb9ad9c1bb4556fa4d73800428f90beb
Neomaster <the@neomaster.net>
2022-05-21 16:19:23 +0300

    0.18.0.0 BulkAllResponseReport.ItemsByIsValid

--- NestClient/Reports/BulkAllResponseReport.cs
+++ NestClient/Reports/BulkAllResponseReport.cs
@@ -7,24 +7,31 @@ public class BulkAllResponseReport
 {
     public long Page;
     public long Retries;
-    public long? CurrentTotal;
     public long ItemsWithErrorsCount;
+    public long? CurrentTotal;
     public Dictionary<int, int> ItemsByStatus;
+    public Dictionary<bool, int> ItemsByIsValid;
     public List<string> ErrorsReasons;
 
     public BulkAllResponseReport(BulkAllResponse r)
     {
+        Page = r.Page;
+        Retries = r.Retries;
+
+        ItemsByStatus = r.Items
+            .GroupBy(k => k.Status, v => v)
+            .ToDictionary(k => k.Key, v => v.Count());
+
+        ItemsByIsValid = r.Items
+            .GroupBy(k => k.IsValid, v => v)
+            .ToDictionary(k => k.Key, v => v.Count());
+
         var errors = r.Items
             .Where(i => i.Error is not null)
             .Select(i => i.Error.Reason);
 
         ItemsWithErrorsCount = errors.Count();
         ErrorsReasons = errors.Distinct().ToList();
-        Page = r.Page;
-        Retries = r.Retries;
-        ItemsByStatus = r.Items
-            .GroupBy(k => k.Status, v => v)
-            .ToDictionary(k => k.Key, v => v.Count());
     }
 
     public BulkAllResponseReport(BulkAllResponse r, ref long currentTotal) : this(r)
