1a4f9a14591103b26b1b07acb3b43241f1c8c993
Neomaster <the@neomaster.net>
2022-05-21 16:50:39 +0300

    0.20.0.0 [BulkIndexDocs_BulkResponseCallback] Tests with updated BulkResponseReport.

--- BulkIndexDocs_BulkResponseCallback/Program.cs
+++ BulkIndexDocs_BulkResponseCallback/Program.cs
@@ -53,16 +53,16 @@ void Test1_InvalidIndexName()
     }
 
     // Output:
-    // {"HasError":true,"CurrentTotal":1000,"ItemsWithErrorsCount":250,"ItemsByStatus":{"400":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"]}
-    // {"HasError":true,"CurrentTotal":500,"ItemsWithErrorsCount":250,"ItemsByStatus":{"400":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"]}
-    // {"HasError":true,"CurrentTotal":250,"ItemsWithErrorsCount":250,"ItemsByStatus":{"400":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"]}
-    // {"HasError":true,"CurrentTotal":750,"ItemsWithErrorsCount":250,"ItemsByStatus":{"400":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"]}
+    // {"IsValid":false,"HasError":true,"ItemsWithErrorsCount":250,"CurrentTotal":750,"ItemsByStatus":{"400":250},"ItemsByIsValid":{"False":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"],"OriginalExceptionMessage":null,"ServerErrorMessage":null}
+    // {"IsValid":false,"HasError":true,"ItemsWithErrorsCount":250,"CurrentTotal":500,"ItemsByStatus":{"400":250},"ItemsByIsValid":{"False":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"],"OriginalExceptionMessage":null,"ServerErrorMessage":null}
+    // {"IsValid":false,"HasError":true,"ItemsWithErrorsCount":250,"CurrentTotal":250,"ItemsByStatus":{"400":250},"ItemsByIsValid":{"False":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"],"OriginalExceptionMessage":null,"ServerErrorMessage":null}
+    // {"IsValid":false,"HasError":true,"ItemsWithErrorsCount":250,"CurrentTotal":1000,"ItemsByStatus":{"400":250},"ItemsByIsValid":{"False":250},"ErrorsReasons":["Invalid index name [te st], must not contain the following characters [ , \", *, \\, <, |, ,, >, /, ?]"],"OriginalExceptionMessage":null,"ServerErrorMessage":null}
     // Error: Refreshing after all documents have indexed failed
 
     // Summary:
     // 1. All batches has been sent but failed.
     // 2. The exception was thrown after all batches were sent.
-    // 3. All responses have status 400.
+    // 3. All responses have status 400, HTTP error and are invalid.
 }
 
 void Test2_CatchingErrors()
@@ -103,7 +103,7 @@ void Test3_ClusterShutdown()
         bulkResponseCallback: bulkResponse =>
         {
             BulkResponseReport m = new(bulkResponse);
-            Console.WriteLine("bulkResponse >>> " + m);
+            Console.WriteLine("bulkResponse    >>> " + m);
         },
         onNext: bulkAllResponse =>
         {
@@ -115,29 +115,27 @@ void Test3_ClusterShutdown()
     }
 
     // Output:
-    // bulkResponse >>> {"HasError":false,...}
-    // bulkAllResponse >>> {...,"ErrorsReasons":[]}
-    // bulkResponse >>> {"HasError":false,...}
-    // bulkAllResponse >>> {...,"ErrorsReasons":[]}
-    // ...
-    // --->                                                                                                                  !
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"An error occurred while sending the request.","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"An error occurred while sending the request.","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"An error occurred while sending the request.","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Подключение не установлено, т.к. конечный компьютер отверг запрос на подключение. (elastic.home:9200)","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
-    // bulkResponse >>> {"HasError":false,"CurrentTotal":null,"ItemsWithErrorsCount":0,"ItemsByStatus":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
-    // Error: BulkAll halted after PipelineFailure.PingFailure from _bulk and exhausting retries (2)
+    //bulkAllResponse >>> {...,"ErrorsReasons":[]}
+    //bulkResponse    >>> {"IsValid":true,"HasError":false,...}
+    //...
+    // see -->            !(1)                                                                                                                                                                !(2)
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"An error occurred while sending the request.","ServerErrorMessage":null}
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Подключение не установлено, т.к. конечный компьютер отверг запрос на подключение. (elastic.home:9200)","ServerErrorMessage":null}
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Подключение не установлено, т.к. конечный компьютер отверг запрос на подключение. (elastic.home:9200)","ServerErrorMessage":null}
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Подключение не установлено, т.к. конечный компьютер отверг запрос на подключение. (elastic.home:9200)","ServerErrorMessage":null}
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
+    //bulkResponse    >>> {"IsValid":false,"HasError":false,"ItemsWithErrorsCount":0,"CurrentTotal":null,"ItemsByStatus":{},"ItemsByIsValid":{},"ErrorsReasons":[],"OriginalExceptionMessage":"Failed to ping the specified node. Call: Status code unknown from: HEAD /","ServerErrorMessage":null}
+    //Error: BulkAll halted after PipelineFailure.PingFailure from _bulk and exhausting retries (2)                                                                                                                !
 
     // Summary:
-    // 1. An exception has been thrown.
-    // 2. No messages from onNext().
-    // 3. No items (ItemsWithErrorsCount, ItemsByStatus). 
-    // 4. All batches before the error occurred were sent. GET /test/_count --> >0
+    // 1. The original exception has been thrown.
+    // 2. The method stopped after exhaustive retries and gave an error about it.
+    // 3. No messages from onNext().
+    // 4. No items (ItemsWithErrorsCount, ItemsByStatus). 
+    // 5. All batches before the error occurred were sent. GET /test/_count --> >0
 }
 
 //Test1_InvalidIndexName();
