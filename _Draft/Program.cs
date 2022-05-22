fca175548adc2131b593397247d55369da6ed21d
Neomaster <the@neomaster.net>
2022-05-22 06:01:50 +0300

    0.29.0.0 _Draft

---
+++ _Draft/Program.cs
@@ -0,0 +1,22 @@
+﻿using Elastic;
+
+#region Connection
+NestClient nestClient = new(
+    new string[] { "https://elastic.home:9200" },
+    "elastic",
+    "ela5tic",
+    "C:/elasticsearch-8.1.1/config/http.p12",
+    "http"
+    );
+
+if (nestClient.Connect(out Exception? e))
+{
+    Console.WriteLine("The connection with elasticsearch was successfully established.");
+}
+else
+{
+    Console.WriteLine(e!.Message);
+    return;
+}
+#endregion
+
