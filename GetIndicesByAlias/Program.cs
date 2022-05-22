67a248f10b28dd78216001536df5c8e9744994fd
Neomaster <the@neomaster.net>
2022-05-22 05:52:26 +0300

    0.28.0.0 GetIndicesByAlias

---
+++ GetIndicesByAlias/Program.cs
@@ -0,0 +1,61 @@
+﻿using Elastic;
+
+#region Connection
+NestClient nestClient = new(
+    new string[] { "https://elastic.home:9200" },
+    "elastic",
+    "ela5tic",
+    "C:/elasticsearch-8.1.1/config/http.p12",
+    "http"
+    );
+
+if (nestClient.Connect(out Exception? e))
+{
+    Console.WriteLine("The connection with elasticsearch was successfully established.");
+}
+else
+{
+    Console.WriteLine(e!.Message);
+    return;
+}
+#endregion
+
+// DELETE *
+// PUT test-1
+// PUT test-2
+// POST _aliases
+// {
+//   "actions": [
+//     {
+//       "add": {
+//         "index": "test*",
+//         "alias": "test"
+//       }
+//     }
+//   ]
+// }
+
+while (true)
+{
+    Console.Write("Alias name: ");
+    
+    string aliasName = Console.ReadLine()!;
+    bool success = nestClient.TryGetIndicesByAlias(aliasName, out string info, out string[] indices);
+
+    Console.WriteLine("Success: " + success);
+    Console.WriteLine("Info: " + info);
+    Console.WriteLine("Indices: " + string.Join(", ", indices));
+    Console.WriteLine();
+}
+
+// Work
+
+// Alias name: test
+// Success: True
+// Info: OK
+// Indices: test-2, test-1
+   
+// Alias name: xxx
+// Success: False
+// Info: Server error: "no such index [xxx]"
+// Indices:
